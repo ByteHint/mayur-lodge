8c7610fbbcd7e0e27a096cc26ec912bc5a614dcf
Anuj Sawant <anujsawant26@gmail.com>
2025-06-21 10:09:10 +0530

    updated hamburger

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { SignInButton, SignUpButton } from '@clerk/nextjs';
 import localFont from 'next/font/local';
+import Hamburger from './Hamburger';
 
 const kugile = localFont({
   src: '../fonts/Kugile_Demo.ttf',
@@ -19,16 +20,18 @@ const aderoTrial = localFont({
 export default function Navbar() {
 
   const scrollToSection = (id: string) => {
-    const scroll = document.getElementById(id);
-    if (scroll) {
-      scroll.scrollIntoView({ behavior: 'smooth' });
+    const elem = document.getElementById(id);
+    if (elem) {
+      elem.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
   return (
     <div className="sticky top-0 z-50 bg-white text-[#3C3C3C] w-full mb-4">
       <div className="w-full mx-auto px-4 sm:px-6 md:px-10 lg:px-16 h-[72px] flex items-center justify-between">
-        
+
+        <Hamburger />
+
         {/* Logo */}
         <div className={`${kugile.className} text-4xl font-normal tracking-widest`}>
           <Link href="/">Mayur Lodge</Link>
@@ -36,20 +39,20 @@ export default function Navbar() {
 
         {/* Nav Links */}
         <div className={`hidden md:flex gap-10 text-sm font-semibold ${aderoTrial.className}`}>
-          <button onClick={() => scrollToSection('about')} className="hover:text-yellow-500 transition uppercase">
+          <button onClick={() => scrollToSection('about')} className="hover:text-yellow-500 hover:scale-105 transition-all delay-150 duration-300 ease-in-out uppercase">
             About Us
           </button>
 
-          <button onClick={() => scrollToSection('facilities')} className="hover:text-yellow-500 transition uppercase">
+          <button onClick={() => scrollToSection('facilities')} className="hover:text-yellow-500 hover:scale-105 transition-all delay-150 duration-300 ease-in-out uppercase">
             Facilities & services
           </button>
-          <button onClick={() => scrollToSection('gallery')} className="hover:text-yellow-500 transition uppercase">
+          <button onClick={() => scrollToSection('gallery')} className="hover:text-yellow-500 hover:scale-105 transition-all delay-150 duration-300 ease-in-out uppercase">
             Gallery
           </button>
-          <button onClick={() => scrollToSection('location')} className="hover:text-yellow-500 transition uppercase">
+          <button onClick={() => scrollToSection('location')} className="hover:text-yellow-500 hover:scale-105 transition-all delay-150 duration-300 ease-in-out uppercase">
             location
           </button>
-          <button onClick={() => scrollToSection('menu')} className="hover:text-yellow-500 transition uppercase">
+          <button onClick={() => scrollToSection('menu')} className="hover:text-yellow-500 hover:scale-105 transition-all delay-150 duration-300 ease-in-out uppercase">
             menu
           </button>
         </div>
@@ -57,7 +60,7 @@ export default function Navbar() {
         {/* Icons + Auth */}
         <div className="flex items-center gap-3">
             {/* User Icon */}
-            <div className="border border-gray-400 rounded-full p-2">
+            <div className="border border-gray-400 rounded-full p-2 hover:scale-105 transition-all delay-150 duration-300 ease-in-out">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -75,7 +78,7 @@ export default function Navbar() {
             </div>
 
             {/* Phone Icon */}
-            <div className="border border-gray-400 rounded-full p-2">
+            <div className="border border-gray-400 rounded-full p-2 hover:scale-105 transition-all delay-150 duration-300 ease-in-out">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -93,7 +96,7 @@ export default function Navbar() {
             </div>
 
             {/* Book Icon */}
-            <div className="border border-gray-400 rounded-full p-2">
+            <div className="border border-gray-400 rounded-full p-2 hover:scale-105 transition-all delay-150 duration-300 ease-in-out">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -114,12 +117,12 @@ export default function Navbar() {
 
         <div className="flex items-center gap-2">
           <SignInButton>
-            <button className="bg-white text-black px-4 py-2 rounded hover:bg-gray-100 transition text-sm border border-gray-300">
+            <button className="bg-white text-black px-4 py-2 rounded hover:bg-gray-100 hover:scale-105 transition-all delay-150 duration-300 ease-in-out text-sm border border-gray-300">
               Sign In
             </button>
           </SignInButton>
           <SignUpButton>
-              <button className="bg-white text-black px-4 py-2 rounded hover:bg-gray-100 transition text-sm border border-gray-300">
+              <button className="bg-white text-black px-4 py-2 rounded hover:bg-gray-100 hover:scale-105 transition-all delay-150 duration-300 ease-in-out text-sm border border-gray-300">
                 Sign Up
               </button>
             </SignUpButton>
