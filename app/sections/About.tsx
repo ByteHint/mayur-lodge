d8d883c43d8eb80423dc6824bd55fb8894e01236
Anuj Sawant <anujsawant26@gmail.com>
2025-06-30 09:34:24 +0530

    updated code

--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -17,18 +17,12 @@ export default function AboutUsPage() {
     <section className="min-h-screen bg-[#FFFFFF] py-16 px-4 sm:px-6 lg:px-8 relative overflow-hidden flex flex-col items-center justify-start pt-28 pb-2">
       <div className="absolute bottom-7 left-15 sm:bottom-12 sm:left-20 md:bottom-16 md:left-25 z-10">
         <h2
-<<<<<<< HEAD
           className={`${kugile.className} text-3xl sm:text-4xl md:text-5xl text-gray-600 whitespace-nowrap select-none `}
-=======
-          className={`${kugile.className} text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-[#3c3c3c]  whitespace-nowrap select-none` }
-          style={{ fontSize: 'clamp(2.5rem, 8vw, 5rem)' }}
->>>>>>> Anuj
         >
           About Us
         </h2>
       </div>
 
-<<<<<<< HEAD
       <div className="max-w-5xl text-[#3C3C3C] mx-auto text-center leading-relaxed text-base sm:text-lg md:text-xl z-20 p-8 sm:p-12 rounded-2xl shadow-md relative overflow-hidden">
         <div className="absolute inset-0 z-0">
           <Image
@@ -39,30 +33,6 @@ export default function AboutUsPage() {
             quality={80}
             className="!static"
           />
-=======
-      {/* Main content container */}
-      <div className="max-w-4xl text-[#3C3C3C] mx-auto text-center leading-relaxed text-base sm:text-lg md:text-xl z-10 bg-[#F3F3F3] p-6 sm:p-8 rounded-2xl shadow-md transition hover:scale-105 duration-300 ease-out"> {/* Adjusted text and padding for responsiveness */}
-        <p className="mb-8">
-          Welcome to Pratik Lodge, your trusted home away from home, strategically located
-          near MIDC Chakan-Kuruli, one of Maharashtra&apos;s most dynamic and rapidly
-          developing industrial zones. Surrounded by major manufacturing giants such as
-          Mercedes-Benz, Volkswagen, Skoda, and Bajaj, we offer unmatched convenience for
-          professionals and travellers alike.
-        </p>
-        <p className="mb-8">
-          Whether you&apos;re visiting for a business assignment, engineering project, or a relaxing
-          getaway, Mayur Lodge caters to every kind of guest—corporate teams, solo
-          adventurers, families, and couples. Our lodge is designed to deliver a clean, peaceful,
-          and budget-friendly experience without compromising on comfort.
-        </p>
-        <p>
-          At Pratik Lodge, we believe in genuine hospitality, efficient service, and a restful
-          atmosphere—making us a preferred stay option in the Chakan-Kuruli area. Whether
-          it&apos;s a short trip or an extended visit, we ensure your stay is smooth, secure, and
-          satisfying.
-        </p>
-      </div>
->>>>>>> Anuj
 
           <div className="absolute inset-0 bg-gray-200 opacity-80"></div>
         </div>
--- app/sections/Facilities.tsx
+++ app/sections/Facilities.tsx
@@ -1,11 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
 
 import { Wifi, UtensilsCrossed } from "lucide-react";
 
-=======
-import {  Wifi, UtensilsCrossed } from "lucide-react";
->>>>>>> Anuj
 import { FaBoxOpen } from "react-icons/fa";
 
 import localFont from "next/font/local";
@@ -20,22 +16,10 @@ const kugile = localFont({
 
 const features = [
   {
-<<<<<<< HEAD
     icon: <Wifi size={32} className="text-[#292929]" />,
 
     title: "Free & Fast Wi-Fi",
 
-=======
-    icon: <FaBoxOpen  size={32} className="text-[#292929]" />,
-    title: "Personalised Service",
-    description:
-      "We offer special personalised services, along with amenities that are tailored according to your preferences and distinct price value.",
-  },
-
-  {
-    icon: <Wifi size={32} className="text-[#292929]" />,
-    title: "Free & Fast Wi-Fi",
->>>>>>> Anuj
     description:
       "Stay seamlessly connected with high-speed internet throughout your stay.",
   },
@@ -61,7 +45,6 @@ const features = [
 
 export default function FacilitiesServices() {
   return (
-<<<<<<< HEAD
     <section
       id="facilities"
       className="bg-[#FFFFFF] text-[#3C3C3C] py-24 px-4 sm:px-6 md:px-12 lg:px-24 min-h-[80vh] flex flex-col justify-center"
@@ -69,10 +52,6 @@ export default function FacilitiesServices() {
       <h2
         className={`${kugile.className} text-3xl sm:text-4xl md:text-5xl mb-10 text-start tracking-wider`}
       >
-=======
-    <section id="facilities" className="bg-[#FFFFFF] text-[#3C3C3C] py-16 px-4 sm:px-6 md:px-12 lg:px-24">
-      <h2 className={`${kugile.className} text-[67px] sm:text-4xl md:text-5xl md:mb-10 mb-2 text-start tracking-wider`}>
->>>>>>> Anuj
         Facilities and Services
       </h2>
 
@@ -81,11 +60,7 @@ export default function FacilitiesServices() {
           {features.map((feature, index) => (
             <div
               key={index}
-<<<<<<< HEAD
               className="rounded-xl p-6 text-start bg-gray-300 shadow-sm transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-lg cursor-pointer"
-=======
-              className="rounded-xl p-6 text-center bg-[#F3F3F3] backdrop-blur-md border border-white/30 shadow-md hover:shadow-lg transition hover:scale-105 duration-300 ease-out"
->>>>>>> Anuj
             >
               <div className="mb-4 flex justify-start">{feature.icon}</div>
 
